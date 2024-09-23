6ac7c0173b816e9c40cc9e9451f361f7ecf8cb00
Flextus <benjasg10@gmail.com>
2024-09-23 11:18:09 -0600

    001_Benjamin_2309
    Clase colisiones

--- test 2D/Assets/player.cs
+++ test 2D/Assets/player.cs
@@ -14,14 +14,29 @@ public class player : MonoBehaviour
         // box.enabled = false;
         box = this.gameObject.GetComponent<BoxCollider2D>();
 
-
-
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Translate(Vector2.right * 1 * Time.deltaTime);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rb.position = new Vector2(transform.position.x - 1, transform.position.y);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+        Debug.Log("ouch");   
+        }
+        Debug.Log("Player touched something....");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
     }
 }
